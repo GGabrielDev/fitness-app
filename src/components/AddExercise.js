22ee29ba7ae0d318a722738e5a0065f860adc032
GGabrielDev <luisgabrielramosrobles@gmail.com>
2020-12-07 01:06:57 -0400

    Refactor based in ES7 & Babel

--- src/components/AddExercise.js
+++ src/components/AddExercise.js
@@ -3,12 +3,10 @@ import btnImg from "../images/add.png";
 import "./styles/AddExercises.css";
 import { Link } from "react-router-dom";
 
-function AddExercise(props) {
-  return (
+const AddExercise = () => (
     <Link to="/exercise/new">
       <img className="Add-Center" src={btnImg} alt="AddExercise" />
     </Link>
-  );
-}
+);
 
 export default AddExercise;
